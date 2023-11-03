fbfdfb857b3ed9e215648be9a68015241361b6e0
search4asraful <asrafulvau009@gmail.com>
2023-11-03 20:43:51 +0600

    disable user input by JavaScritp and allign items middle of viewport

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const passBox = document.getElementById("password");
 const lenth = 12;
 
+// remember if iam using "readonly" attribute on markup than enable and disable not nessecary in JavaScript
+document.getElementById("password").disabled = true;
+// disable user input
+
 const upercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const number = "1234567890";
@@ -17,6 +21,12 @@ const allChars = upercase + lowercase + number +symbol;
     };
 
     function copyPassword(){
+        passBox.removeAttribute('disabled');
+        // temporary enable user input
+
         passBox.select();
-        document.execCommand("copy")
+        document.execCommand("copy");
+
+        passBox.setAttribute('disabled', true);
+        // than disable user input again
     };
