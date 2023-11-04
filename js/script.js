a5250c687a3227d7f0e28d909fc622e44a2c53ee
search4asraful <asrafulvau009@gmail.com>
2023-11-04 17:05:32 +0600

    add popup msg on copy

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const passBox = document.getElementById("password");
-<<<<<<< HEAD
 const copyimg = document.querySelector(".input_field img");
 
 // remember if using "readonly" attribute on markup than enable and disable not nessecary in JavaScript
@@ -10,18 +9,7 @@ const upercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const number = "1234567890";
 const symbol = "!@#$%^&*()_-+{}[]/";
-=======
-const lenth = 12;
 
-// remember if iam using "readonly" attribute on markup than enable and disable not nessecary in JavaScript
-document.getElementById("password").disabled = true;
-// disable user input
-
-const upercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowercase = "abcdefghijklmnopqrstuvwxyz";
-const number = "1234567890";
-const symbol = "!@#$%&*()__+{}[]/";
->>>>>>> fbfdfb857b3ed9e215648be9a68015241361b6e0
 const allChars = upercase + lowercase + number +symbol;
 
     function randomPassword(){
@@ -31,10 +19,8 @@ const allChars = upercase + lowercase + number +symbol;
             password += allChars[Math.floor(Math.random() * allChars.length)];
         }
         passBox.value = password;
-<<<<<<< HEAD
+
         copyimg.style.display = "block";
-=======
->>>>>>> fbfdfb857b3ed9e215648be9a68015241361b6e0
     };
 
     function copyPassword(){
@@ -46,7 +32,6 @@ const allChars = upercase + lowercase + number +symbol;
 
         passBox.setAttribute('disabled', true);
         // than disable user input again
-<<<<<<< HEAD
 
         var popup = document.getElementById("popup_overlay");
 
@@ -58,6 +43,4 @@ const allChars = upercase + lowercase + number +symbol;
             copyimg.style.display = "none";
             popup.classList.remove("popup_show");
         }, 3000);
-=======
->>>>>>> fbfdfb857b3ed9e215648be9a68015241361b6e0
     };
