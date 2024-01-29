13f7d9914c1e39f65fe61ce75a6931cd2693ce3a
search4asraful <asrafulvau009@gmail.com>
2024-01-29 22:38:55 +0600

    using Session storage for store and show generated password

--- js/script.js
+++ js/script.js
@@ -1,26 +1,48 @@
 const passBox = document.getElementById("password");
 const copyimg = document.querySelector(".input_field img");
+const passwordList = document.getElementById("passwordList");
 
 // remember if using "readonly" attribute on markup than enable and disable not nessecary in JavaScript
 document.getElementById("password").disabled = true;
 
-const lenth = 12;
-const upercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowercase = "abcdefghijklmnopqrstuvwxyz";
-const number = "1234567890";
-const symbol = "!@#$%^&*()_-+{}[]/";
 
-const allChars = upercase + lowercase + number +symbol;
+function randomPassword(){
+    const length = parseInt(document.getElementById("length").value, 10);
+    const upercase = document.getElementById("uppercase").checked
+      ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      : "";
+    const lowercase = document.getElementById("lowercase").checked
+      ? "abcdefghijklmnopqrstuvwxyz"
+      : "";
+    const number = document.getElementById("numbers").checked ? "1234567890" : "";
+    const symbol = document.getElementById("symbols").checked
+      ? "!@#$%^&*()_-+{}[]/"
+      : "";
+    
+    const allChars = upercase + lowercase + number +symbol;
 
-    function randomPassword(){
-        let password = "";
+    if (allChars.length === 0) {
+        alert("Please select at least one character set.");
+        return;
+      }
+      
+    let password = "";
         
-        while(lenth > password.length){
+        while(length > password.length){
             password += allChars[Math.floor(Math.random() * allChars.length)];
         }
+
+        // Store password in session storage
+        let passwordsHistory = JSON.parse(sessionStorage.getItem("passwords")) || [];
+        passwordsHistory.push(password);
+        sessionStorage.setItem("passwords", JSON.stringify(passwordsHistory));
+
         passBox.value = password;
 
         copyimg.style.display = "block";
+
+        // Call display password history function
+        displayPasswordHistory();
     };
 
     function copyPassword(){
@@ -35,7 +57,7 @@ const allChars = upercase + lowercase + number +symbol;
 
         var popup = document.getElementById("popup_overlay");
 
-        popup.innerHTML = "<p>Password copied Successfully <br/> <span>Paste where you want</span></p>";
+        popup.innerHTML = "<span>Copied</span>";
 
         popup.classList.add("popup_show");
         setTimeout(function() {
@@ -44,3 +66,28 @@ const allChars = upercase + lowercase + number +symbol;
             popup.classList.remove("popup_show");
         }, 3000);
     };
+    
+    function displayPasswordHistory() {
+        // Retrieve password history from session storage
+        let passwordsHistory = JSON.parse(sessionStorage.getItem("passwords")) || [];
+        
+        // Display passwords in the passwordList span
+        passwordList.innerHTML = passwordsHistory
+        .reverse()
+        .map((password) => `<span>${password}</span><br><br>`)
+        .join("");
+        
+        if (passwordsHistory.length > 0) {
+            passwordHistory.style.display = "block";
+        }else{
+            passwordHistory.style.display = "none";
+        }
+      }
+      function clearHistory() {
+        // Clear session storage and update password history
+        sessionStorage.removeItem("passwords");
+        displayPasswordHistory();
+      }
+
+    // Display password history on page load
+    displayPasswordHistory();
